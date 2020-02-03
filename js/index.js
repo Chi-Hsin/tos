846b74d9684d53047b34b38ffe3691078c3e090e
Chi-Hsin <jack1234552000@yahoo.com.tw>
2020-02-03 17:58:42 +0800

    202002031757
    滑鼠圖標置入
    移動的狀況下  更易觀察

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ var indexData = new Vue({
                     'img/fire.png',
                     'img/tree.png'
                 ],
-                'elementSelect':'heart',
+                'elementSelect':'img/heart.png',
                 'keyCodeCommand':{
                     'ArrowRight':'light',
                     'ArrowLeft':'heart',
@@ -23,47 +23,60 @@ var indexData = new Vue({
                     'width':'100%',
                     'min-height':'300px'
                 },
+                'mousePicDisplay':false,
+                'mousePicSrc':"img/fire.png",
+                'mousePicStyle':{
+                    'position':'absolute',
+                    'left':0,
+                    'top':0,
+                    'display':'none',
+                },
+                'elementTableStyle':{
+                    'width':'660px',
+                    'height':'550px',
+                    'cursor':'default'
+                },
                 'boardTemplate':[],//暫存板子上的資料
             	'elementBoard':[
                            //第一排珠
-                            {index:0,element:'img/heart.png',background:"#3E2615",type:"normal"},
-                            {index:1,element:'img/heart.png',background:"#28180F",type:"normal"},
-                            {index:2,element:'img/heart.png',background:"#3E2615",type:"normal"},
-                            {index:3,element:'img/heart.png',background:"#28180F",type:"normal"},
-                            {index:4,element:'img/heart.png',background:"#3E2615",type:"normal"},
-                            {index:5,element:'img/heart.png',background:"#28180F",type:"normal"},
+                            {index:0,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
+                            {index:1,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
+                            {index:2,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
+                            {index:3,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
+                            {index:4,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
+                            {index:5,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
                         
                           //第二排珠
-                            {index:6,element:'img/heart.png',background:"#28180F",type:"normal"},
-                            {index:7,element:'img/heart.png',background:"#3E2615",type:"normal"},
-                            {index:8,element:'img/heart.png',background:"#28180F",type:"normal"},
-                            {index:9,element:'img/heart.png',background:"#3E2615",type:"normal"},
-                            {index:10,element:'img/heart.png',background:"#28180F",type:"normal"},
-                            {index:11,element:'img/heart.png',background:"#3E2615",type:"normal"},
+                            {index:6,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
+                            {index:7,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
+                            {index:8,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
+                            {index:9,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
+                            {index:10,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
+                            {index:11,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
                         
                           //第三排珠
-                            {index:12,element:'img/heart.png',background:"#3E2615",type:"normal"},
-                            {index:13,element:'img/heart.png',background:"#28180F",type:"normal"},
-                            {index:14,element:'img/heart.png',background:"#3E2615",type:"normal"},
-                            {index:15,element:'img/heart.png',background:"#28180F",type:"normal"},
-                            {index:16,element:'img/heart.png',background:"#3E2615",type:"normal"},
-                            {index:17,element:'img/heart.png',background:"#28180F",type:"normal"},
+                            {index:12,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
+                            {index:13,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
+                            {index:14,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
+                            {index:15,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
+                            {index:16,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
+                            {index:17,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
                         
                            //第四排珠
-                            {index:18,element:'img/heart.png',background:"#28180F",type:"normal"},
-                            {index:19,element:'img/heart.png',background:"#3E2615",type:"normal"},
-                            {index:20,element:'img/heart.png',background:"#28180F",type:"normal"},
-                            {index:21,element:'img/heart.png',background:"#3E2615",type:"normal"},
-                            {index:22,element:'img/heart.png',background:"#28180F",type:"normal"},
-                            {index:23,element:'img/heart.png',background:"#3E2615",type:"normal"},
+                            {index:18,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
+                            {index:19,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
+                            {index:20,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
+                            {index:21,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
+                            {index:22,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
+                            {index:23,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
                         
                            //第五排珠
-                            {index:24,element:'img/heart.png',background:"#3E2615",type:"normal"},
-                            {index:25,element:'img/heart.png',background:"#28180F",type:"normal"},
-                            {index:26,element:'img/heart.png',background:"#3E2615",type:"normal"},
-                            {index:27,element:'img/heart.png',background:"#28180F",type:"normal"},
-                            {index:28,element:'img/heart.png',background:"#3E2615",type:"normal"},
-                            {index:29,element:'img/heart.png',background:"#28180F",type:"normal"},
+                            {index:24,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
+                            {index:25,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
+                            {index:26,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
+                            {index:27,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
+                            {index:28,element:'img/heart.png',background:"#3E2615",type:"normal",opacity:1},
+                            {index:29,element:'img/heart.png',background:"#28180F",type:"normal",opacity:1},
                         
                     ],
                     'handMotionOrigin':0,//紀錄離開的轉點位置
@@ -91,18 +104,22 @@ var indexData = new Vue({
                 },
                 test1:function(e){//滑鼠離開元素觸發
                     var index = e.target.getAttribute("data-index");
+                    this.elementBoard[index].opacity = 1;
                     this.handMotionOrigin = Number(index);
                     // console.log("離開第"+this.handMotionOrigin+"個元素");
 
                 },
                 test2:function(e){//滑鼠觸碰元素觸發
                     this.indexLocate = e.target.getAttribute("data-index");
+                    
 
                     if(this.handMotionContinue){//當滑鼠按著不放時  才交換元素
                         if(this.handMotionOver == Number(this.indexLocate)){return;}
                         this.handMotionOver = Number(this.indexLocate);
+                        this.elementBoard[this.indexLocate].opacity = 0.5;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
                         // console.log("接觸第"+this.handMotionOver+"個元素");
                         this.test3()
+                        this.elementTableStyle.cursor = "url('img/fire.png'),auto";
                     }
                 },
                 test3:function(e){//交換元素
@@ -112,13 +129,18 @@ var indexData = new Vue({
                 },
                 //滑鼠按著不放+滑鼠放開的動作  其實就是click
                 test4:function(e){//滑鼠按著不放
+                    var index = e.target.getAttribute("data-index");
+
                     if(this.handMotionContinue == 1){ //如果是移動狀態  又跑到這個狀態時 那麼就設定滑鼠放開的狀態 而不改色
                         this.handMotionContinue = 0;
+                        this.elementBoard[index].opacity = 1;
+                        this.elementTableStyle.cursor = "default";
                         console.log("移動狀態解除");
                         return;
                     }
-                    var index = e.target.getAttribute("data-index");
+
                     this.handMotionOrigin = Number(index); //按著不放會同時觸發自己珠子上的觸碰事件  這樣做可與自己交換
+                    this.elementBoard[index].opacity = 0.5;
                     this.handMotionContinue = 1;
                     console.log("滑鼠按著不放")
                 },
@@ -130,6 +152,13 @@ var indexData = new Vue({
                         console.log("珠子已換色");
                     }
                 },
+                // mousePic:function(e){//圖片跟著滑鼠移動 廢除 已改用cursor屬性更方便
+                   
+                   
+                //    this.mousePicStyle.left = e.clientX +30+ "px";
+                //    this.mousePicStyle.top = e.clientY +30+ "px";
+                //    console.log("圖片跟著滑鼠移動",",X=",e.clientX,",Y=",e.clientY);
+                // },
                 boardAll:function(obj){
                     for(var i=0;i<5;i++){
                         for(var j=0;j<6;j++){
